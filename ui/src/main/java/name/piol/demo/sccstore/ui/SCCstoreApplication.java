397f51c72fe2b19304e60a89a9cbbdae8ca692ec
Domenico Piol <domenico@piol.name>
2022-07-03 20:56:25 +0200

    spring kubernetes

--- ui/src/main/java/name/piol/demo/sccstore/ui/SCCstoreApplication.java
+++ ui/src/main/java/name/piol/demo/sccstore/ui/SCCstoreApplication.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
 @SpringBootApplication
-@EnableDiscoveryClient
 public class SCCstoreApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
