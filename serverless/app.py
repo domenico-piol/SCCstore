e5c41367b001f087ec94182af2a13ee2848fb688
Domenico Piol <domenico@piol.name>
2022-07-04 11:53:44 +0200

    serverless

--- serverless/app.py
+++ serverless/app.py
@@ -6,33 +6,25 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    print("starting...")
-    
+def hello_world():    
     conn = psycopg2.connect(
         host="win10-postgresql",
         database="sccstore",
         user="sccstore",
         password="sccstore")
-    
-    print("Connected to database")
-    
+        
     cur = conn.cursor()
-    print("got a cursor")
 
     cur.execute('SELECT * FROM compliants;')
-    print('executed select')
     dataarray = cur.fetchall()
-
-    print (dataarray)
 
     cur.close()
     conn.close()
 
-    #return complaints
+    return dataarray
 
-    target = os.environ.get('TARGET', 'World')
-    return 'Hello {}!\n'.format(target)
+    #target = os.environ.get('TARGET', 'World')
+    #return 'Hello {}!\n'.format(target)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
