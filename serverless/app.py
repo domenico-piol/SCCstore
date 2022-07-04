e47e6c84bfdfc81108dcaf5a1f71a59ce6034241
Domenico Piol <domenico@piol.name>
2022-07-04 10:56:25 +0200

    serverless

--- serverless/app.py
+++ serverless/app.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 
 from flask import Flask
 
@@ -6,8 +7,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    target = os.environ.get('TARGET', 'World')
-    return 'Hello {}!\n'.format(target)
+    conn = psycopg2.connect(
+        host="win10-postgresql",
+        database="sccstore",
+        user=os.environ['sccstore'],
+        password=os.environ['sccstore'])
+
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM compliants;')
+    complaints = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return complaints
+
+    #target = os.environ.get('TARGET', 'World')
+    #return 'Hello {}!\n'.format(target)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
