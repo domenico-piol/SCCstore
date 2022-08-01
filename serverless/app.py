fc2d53013d9b67e10a98145ea8c85d10cc2505b1
Domenico Piol <domenico@piol.name>
2022-08-01 17:17:38 +0200

    serverless configmap

--- serverless/app.py
+++ serverless/app.py
@@ -2,7 +2,7 @@ import os
 import psycopg2
 
 from os import environ
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 app.config['SCC_DB_HOST'] = environ.get('SCC_DB_HOST')
@@ -11,8 +11,8 @@ app.config['SCC_DB_HOST'] = environ.get('SCC_DB_HOST')
 @app.route('/')
 def hello_world():    
     conn = psycopg2.connect(
-        host="win10-postgresql",
-        # host=app.config['SCC_DB_HOST'],
+        #host="win10-postgresql",
+        host=app.config['SCC_DB_HOST'],
         database="sccstore",
         user="sccstore",
         password="sccstore")
@@ -34,8 +34,27 @@ def hello_world():
 
     return htmlPageHeader + htmlPageContent + htmlPageFooter
 
-    #target = os.environ.get('TARGET', 'World')
-    #return 'Hello {}!\n'.format(target)
+@app.route('/complaint', methods=['POST'])
+def write_complaint():
+    complaint_value = str(request.form['compl'])
+
+    conn = psycopg2.connect(
+        #host="win10-postgresql",
+        host=app.config['SCC_DB_HOST'],
+        database="sccstore",
+        user="sccstore",
+        password="sccstore")
+        
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO complaints (complaint) VALUES (%s);", (complaint_value,))
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    return complaint_value, 200
+
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
