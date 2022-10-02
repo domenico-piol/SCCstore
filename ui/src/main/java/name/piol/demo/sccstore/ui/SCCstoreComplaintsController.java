64ffa7a80d86de64903c8a8e7a33258ce99b40eb
Domenico Piol <domenico@piol.name>
2022-10-02 16:15:35 +0200

    test webclient

--- ui/src/main/java/name/piol/demo/sccstore/ui/SCCstoreComplaintsController.java
+++ ui/src/main/java/name/piol/demo/sccstore/ui/SCCstoreComplaintsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +16,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 import name.piol.demo.sccstore.ui.datamodel.Complaint;
 //import name.piol.demo.sccstore.ui.datamodel.ComplaintsRepository;
 import reactor.core.publisher.Flux;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
 
 @Controller
 public class SCCstoreComplaintsController {
@@ -34,7 +37,14 @@ public class SCCstoreComplaintsController {
         //List<Complaint> complaints = (List<Complaint>) complaintRepository.findAll();
 
         try {
-            WebClient webClient = WebClient.builder().baseUrl(complaintsBackend).build();
+            ConnectionProvider provider = ConnectionProvider.builder("fixed")
+                .maxConnections(500)
+                .maxIdleTime(Duration.ofSeconds(20))
+                .maxLifeTime(Duration.ofSeconds(60))
+                .pendingAcquireTimeout(Duration.ofSeconds(60))
+                .evictInBackground(Duration.ofSeconds(120)).build();
+            
+            WebClient webClient = WebClient.builder().baseUrl(complaintsBackend).clientConnector(new ReactorClientHttpConnector(HttpClient.create(provider))).build();
 
             Flux<Complaint> complaintsFlux = webClient.get().uri("/complaints").retrieve().bodyToFlux(Complaint.class);   
             List<Complaint> complaints = complaintsFlux.collect(Collectors.toList()).share().block(); 
